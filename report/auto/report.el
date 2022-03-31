ec8eb01b7afcd3dad4642ef98caa00e429886f69
wmacmil <gusmacwa@student.gu.se>
2022-03-31 23:10:18 +0200

    trying pstricks

--- report/auto/report.el
+++ report/auto/report.el
@@ -4,14 +4,14 @@
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("article" "a4paper" "11pt")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
-                     '(("biblatex" "mincrossrefs=999" "style=numeric" "backend=biber" "url=false" "isbn=false" "doi=false" "") ("geometry" "margin=1in") ("xcolor" "dvipsnames") ("hyperref" "colorlinks")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+                     '(("biblatex" "mincrossrefs=999" "style=numeric" "backend=biber" "url=false" "isbn=false" "doi=false" "") ("geometry" "margin=1in") ("xcolor" "dvipsnames") ("hyperref" "colorlinks") ("pstricks" "usenames" "dvipsnames") ("grffile" "space")))
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -31,7 +31,13 @@
     "fontspec"
     "titlesec"
     "graphicx"
-    "float")
+    "float"
+    "pstricks"
+    "epsfig"
+    "pst-grad"
+    "pst-plot"
+    "grffile"
+    "etoolbox")
    (LaTeX-add-labels
     "fig:M1"
     "fig:M2"
